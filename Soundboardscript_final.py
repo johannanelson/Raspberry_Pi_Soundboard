36216ce7fcfd129603d8353dd2718b5e46bdc7a7
johannanelson <johannanelson99@gmail.com>
2016-04-13 17:38:51 -0400

    Added second button

--- Soundboardscript_final.py
+++ Soundboardscript_final.py
@@ -4,7 +4,6 @@ from pygame.mixer import Sound
 from signal import pause
 import time
 
-#this is a comment
 pygame.init()
 pygame.mixer.init()
 SOUND_END = pygame.USEREVENT+1
@@ -15,7 +14,10 @@ current_sound_button_object = -1
 objs = [
 
 { "buttonPin" : 24, "ledPin" : 17, "soundFile":
-"/home/pi/desktop/wolves.wav"},
+"/home/pi/Desktop/wolves.wav"},
+        
+{ "buttonPin" : 4, "ledPin" : 23, "soundFile":
+"/home/pi/Desktop/explosion.wav"},
 
 ]
 
@@ -37,12 +39,12 @@ class SoundButton:
                self.button.when_pressed = self.pressed
 
        def pressed(self):
-               print("pressed")
+               #print("pressed")
+               self.led.on()
+               time.sleep(5)
                pygame.mixer.music.load(self.sound_file)
                pygame.mixer.music.play()
-	       soundButtons [current_sound_button_object].turn_off_led()
-               self.led.on()
-	       current_sound_button_object = self.id
+               self.led.off()
 	       
        def turn_off_led(self):
                self.led.off()
@@ -58,17 +60,30 @@ for index, item in enumerate(objs):
 
 for button in soundButtons:
    button.led.on()
-   print 'led on'
    time.sleep(0.15)
+
+time.sleep(2)
 
 for button in soundButtons:
    button.led.off()
-   print 'led off'
    time.sleep(0.15)
 
 print("button startup done")
 
 while (True):
+    button.when_pressed = button.pressed()
+    button.when_released = button.turn_off_led()
+    pause()
+
+
+
+
+
+
+
+
+
+'''while (True):
        time.sleep(1)
        for event in pygame.event.get():
 		if event.type == SOUND_END:
@@ -83,7 +98,7 @@ while (True):
 				soundButtons[current_sound_button_object].turn_off_led()
 				current_sound_button_object = -1
 
-pause()
+pause()'''
 
 
 	
